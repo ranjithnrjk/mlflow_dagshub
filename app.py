e30419b883d36e6e5bb9e7857f0f9028971b643b
N Ranjith kumar <109662378+ranjithnrjk@users.noreply.github.com>
2024-09-15 08:36:48 +0000

    13_09_24

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ class_report = classification_report(y_test, y_pred, output_dict=True)
 print(class_report)
 
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+mlflow.set_tracking_uri("http://127.0.0.1:5000/git")
 mlflow.set_experiment("Breast Cancer")
 
 mlflow.log_params(params)
